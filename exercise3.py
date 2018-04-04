df4cc1319bf2897e33113ce22aee82dad3ef364c
Mike Quaid <36292540+mikequaid@users.noreply.github.com>
2018-04-04 08:40:41 +0100

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,7 +1,7 @@
 #Mike Quaid 15/02/18
 #Exercise 3
 #Collatz Conjecture (https://en.wikipedia.org/wiki/Collatz_conjecture)
-#start with any positive integer n.
+#Start with any positive integer n.
 #Then each term is obtained from the previous term as follows: if the previous term is #even, the next term is one half the previous term.
 #Otherwise, the next term is 3 times the previous term plus 1.
 #The onjecture is that no matter what value of n, the sequence will always reach 1
