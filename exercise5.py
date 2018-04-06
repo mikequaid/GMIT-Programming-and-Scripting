fb9d6d9cef5329b824a163c6d29b2a4b4ce3b656
Mike Quaid <36292540+mikequaid@users.noreply.github.com>
2018-04-06 16:57:15 +0100

    Update exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -2,10 +2,10 @@
 # Exercise 5
 # script that reads the Iris data set in
 # and prints the four numerical values on each row in a nice format
+# replace:
+# https://docs.python.org/3/library/stdtypes.html?highlight=replace#str.replace
 
-with open("data/iris2.csv") as x:
-  for line in x:  
-    print(line.split(',')[:4])
-    type(line)
-
-# print('{:2d} {:3d} {:4d} {:5d}'.format(x,x**2,x**3,x**4))
+with open("data/iris2.csv") as y:
+    for line in y: #loop through each line in the data set
+        line = line.replace(',', ' ') # using string method replace "," with space
+        print(line[0:16].strip()) # print elements 0 to 16 and strip out all other elements of the string
