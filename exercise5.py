f58f6bc73c08a55932cea17c79c55d8b4e20a2a6
Mike Quaid <36292540+mikequaid@users.noreply.github.com>
2018-04-07 15:07:25 +0100

    Update exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -1,11 +1,12 @@
 # Mike Quaid 19032018
 # Exercise 5
-# script that reads the Iris data set in
+# script that reads the Iris data set into python
 # and prints the four numerical values on each row in a nice format
 # replace:
 # https://docs.python.org/3/library/stdtypes.html?highlight=replace#str.replace
 
-with open("data/iris2.csv") as y:
+with open("data/iris.txt") as y:
     for line in y: #loop through each line in the data set
         line = line.replace(',', ' ') # using string method replace "," with space
-        print(line[0:16].strip()) # print elements 0 to 16 and strip out all other elements of the string
+        print(line[0:16].strip()) # print elements 0 to 16 and strip all other elements
+ 
